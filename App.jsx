b88c5a505cff08407d15d45fa4f822fd88daec1c
Sanjana432 <sanjana.sundar.k@gmail.com>
2025-06-17 20:33:12 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AboutSection from './components/AboutSection';
+import HistorySection from './components/HistorySection';
+
+function App() {
+  return (
+    <div>
+      <AboutSection />
+      <HistorySection />
+    </div>
+  );
+}
+
+export default App;
